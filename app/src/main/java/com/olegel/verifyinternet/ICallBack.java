2c491598e3271c88e85f9cc75b96bfb3b4b4c0a6
andrteamgear <andrteamgear@gmail.com>
2017-07-19 10:04:39 +0300

    Remove app folder

--- app/src/main/java/com/olegel/verifyinternet/ICallBack.java
+++
@@ -1,9 +0,0 @@
-package com.olegel.verifyinternet;
-
-/**
- * Created by Oleg on 19.07.2017.
- */
-
-public interface ICallBack {
-    void requestCallBack(boolean connectionResult, int responseCode);
-}
