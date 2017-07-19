a3737ce6f289f0887eb2c612e88c3a6fc38b0bba
andrteamgear <andrteamgear@gmail.com>
2017-07-19 09:44:26 +0300

    First release

---
+++ app/src/main/java/com/olegel/verifyinternet/ICallBack.java
@@ -0,0 +1,9 @@
+package com.olegel.verifyinternet;
+
+/**
+ * Created by Oleg on 19.07.2017.
+ */
+
+public interface ICallBack {
+    void requestCallBack(boolean connectionResult, int responseCode);
+}
---
+++ checkinternet/src/main/java/com/olegel/checkinternet/ICallBack.java
@@ -0,0 +1,9 @@
+package com.olegel.checkinternet;
+
+/**
+ * Created by Oleg on 19.07.2017.
+ */
+
+public interface ICallBack {
+    void requestCallBack(boolean connectionResult, int responseCode);
+}
